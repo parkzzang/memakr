986fb384f07e8d461e476d10bebbd848bbe895f0
parkzzang <parkzzang052@naver.com>
2025-04-09 00:35:30 +0900

    svg 처리 기능

--- main/forms.py
+++ main/forms.py
@@ -5,8 +5,12 @@ from django.contrib.auth.models import User
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
+    agree_terms = forms.BooleanField(required=True, label="이용약관 동의")
+    agree_privacy = forms.BooleanField(required=True, label="개인정보처리방침 동의")
 
     class Meta:
         model = User
         fields = ("username", "password1", "password2", "email")
 
+    def clean(self):
+        cleaned_data = super().clean()
